a7937fcf582a9bacfd02b19f03627608d8c349d5
Jeff Mobley <njg26crux@gmail.com>
2017-10-31 09:47:37 -0700

    fixed pg server issues

--- app/public/js/components/root.component.js
+++ app/public/js/components/root.component.js
@@ -35,56 +35,18 @@
 
     vm.login = function(){
       console.log(vm.form)
-      $http.get('/token', vm.form).then(); //console.log('Holy Crap Batman !!!')
+      $http.get('/token', vm.form).then();
       $mdDialog.hide();
       delete vm.form;
     }
 
     vm.signup = function(){
       console.log(vm.form)
-      $http.post('/users', vm.form).then(); //.fail(console.log(err)) console.log(response.data)
-
-    //   const options = {
-    //     contentType: 'application/json',
-    //     data: JSON.stringify({ firstName, lastName, email, userName, password }),
-    //     dataType: 'json',
-    //     type: 'POST',
-    //     url: '/users'
-    //   };
-    //
-    //   $.ajax(options)
-    //     .done((user) => {
-    //       window.location.href = '/jobs.html';
-    //     })
-    //     .fail(($xhr) => {
-    //       Materialize.toast($xhr.responseText, 3000);
-    //     });
-    // });
-
+      $http.post('/users', vm.form).then();
       $mdDialog.hide();
       delete vm.form;
     }
 
-    // vm.answer = function (answer) {
-    //   console.log('hello')
-    //   if(vm.form){
-    //     console.log('First Name: ' + vm.form.firstname);
-    //     console.log('Last Name: ' + vm.form.lastname);
-    //     console.log('User Name: ' + vm.form.username);
-    //     console.log('email: ' + vm.form.email);
-    //     console.log('Password: ' + vm.form.password);
-    //     console.log('Password Again: ' + vm.form.passwordagain);
-    //   }
-    //
-    //   if (answer === 'SignUp') {
-    //     console.log('answer: ' + answer);
-    //     // vm.logIn = !vm.logIn;
-    //     delete vm.form;
-    //     return;
-    //   }
-    //
-    //     // $mdDialog.hide(answer);
-    // };
   }])
 
 }());
