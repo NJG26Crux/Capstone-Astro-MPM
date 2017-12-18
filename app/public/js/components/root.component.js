3564782dd4b7af26ebc05d33c1d23fcd386b8cd3
Jeff Mobley <njg26crux@gmail.com>
2017-12-18 11:12:13 -0800

    another commit

--- app/public/js/components/root.component.js
+++ app/public/js/components/root.component.js
@@ -27,7 +27,6 @@
           }
         })
         .catch((err) => {
-          // console.log(err)
         });
     }
 
@@ -55,15 +54,12 @@
     };
 
     vm.logout = function() {
-      // console.log('logging out');
       $http.delete('/api/token').then(data => {
         vm.auth.user = '';
         auth.firstName = '';
         auth.err = '';
-        // console.log('data: ', data);
       });
       $state.go('home')
-      // console.log('logged out?');
     }
   }
 
@@ -71,34 +67,23 @@
     const vm = this;
     $scope.form = {};
 
-    // vm.firstName = '';
     vm.footer = true; //***************** NEW
     console.log('vm.footer: ',vm.footer); //***************** NEW
     vm.logIn = true;
     vm.loggedin = false; // for nav bar
-    // vm.firstName = "";
 
     vm.close = function() {
       console.log('close')
       $mdDialog.cancel();
     };
-    // vm.hide = function() {
-    //   console.log('hide');
-    //   $mdDialog.hide();
-    // };
 
     vm.login = function() {
-      // console.log('fun.login: ', vm.form)
       $http.post('/api/token', vm.form).then(data => {
-          // console.log('data: ', data)
-          // console.log('firstName: ', data.data.firstName);
           auth.firstName = 'Hello ' + data.data.firstName;
-          // console.log('auth.firstName: ', auth.firstName);
           $mdDialog.hide();
           delete vm.form;
         },
         err => {
-          // console.log('err: ', err)
           vm.err = 'Incorect User Name or Password'
           return err;
         })
@@ -108,13 +93,11 @@
     vm.signup = function() {
       console.log('fun.signup: ', vm.form)
       $http.post('/api/users', vm.form).then(user => {
-          // if (user.data.firstName) {
           console.log('signup.user.data: ', user.data);
           auth.firstName = 'Hello ' + user.data.firstName;
           console.log('auth.firstName: ', auth.firstName);
           $mdDialog.hide();
           delete vm.form;
-          // }
         },
         err => {
           console.log('err: ', err)
@@ -123,56 +106,32 @@
         })
     };
 
-    vm.logInState = function() {
+    vm.switchToSignup = function(ev) {
+      console.log('clicked on switch to signupForm fun');
+      vm.close();
+      console.log('clicked on signupForm fun');
+      $mdDialog.show({
+        controller: 'login as $ctrl',
+        templateUrl: 'js/components/signup.template.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        fullscreen: this.customFullscreen
+      });
+    };
 
-      if (vm.logIn === true) {
-        console.log('clearing all login fields');
-        vm.err = '';
-        delete vm.form;
-        vm.logIn = !vm.logIn;
-
-        $scope.form.userName.$pristine = true;
-        $scope.form.userName.$dirty = false;
-        $scope.form.userName.$valid = true;
-        $scope.form.userName.$invalid = false;
-
-        $scope.form.password.$pristine = false;
-        $scope.form.password.$dirty = false;
-        $scope.form.password.$valid = false;
-        $scope.form.password.$invalid = false;
-
-      } else if (vm.logIn === false) {
-        console.log('clearing all signup fields');
-        vm.err = '';
-        delete vm.form;
-        vm.logIn = !vm.logIn;
-
-        $scope.form.firstName.$pristine = true;
-        $scope.form.firstName.$dirty = false;
-        $scope.form.firstName.$valid = true;
-        $scope.form.firstName.$invalid = false;
-
-        $scope.form.lastName.$pristine = false;
-        $scope.form.lastName.$dirty = false;
-        $scope.form.lastName.$valid = false;
-        $scope.form.lastName.$invalid = false;
-
-        $scope.form.userName.$pristine = true;
-        $scope.form.userName.$dirty = false;
-        $scope.form.userName.$valid = true;
-        $scope.form.userName.$invalid = false;
-
-        $scope.form.email.$pristine = false;
-        $scope.form.email.$dirty = false;
-        $scope.form.email.$valid = false;
-        $scope.form.email.$invalid = false;
-
-        $scope.form.password.$pristine = false;
-        $scope.form.password.$dirty = false;
-        $scope.form.password.$valid = false;
-        $scope.form.password.$invalid = false;
-      }
-    }
+    vm.switchToLogin = function(ev) {
+      console.log('clicked on switch to loginForm fun');
+      vm.close();
+      $mdDialog.show({
+        controller: 'login as $ctrl',
+        templateUrl: 'js/components/login.template.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        fullscreen: this.customFullscreen
+      });
+    };
 
   }])
 
